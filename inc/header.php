4610ae374ddbde02f96c828af9888a95028fb10c
GmanyeAnanias1 <ananiasgmanye0@gmail.com>
2025-03-23 15:18:52 -0700

    Added hover effects to the cards elements ont the homepage

--- inc/header.php
+++ inc/header.php
@@ -8,6 +8,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+
   <title>Leave Feedback</title>
 
   <style>
